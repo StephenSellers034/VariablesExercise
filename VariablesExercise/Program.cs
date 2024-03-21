f4cc90d1c4b26d6d5e2af12f61c174159e0f9ba9
Stephen Sellers <stephensellers034@yahoo.com>
2024-03-21 15:34:39 -0400

    VariablesExercise

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace VariablesExercise;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "John";
+        int age = 34;
+        char A = 'A';
+        bool Tr = true;
+        double payment = 16.50;
+        decimal tax = 0.5m;
+
+        Console.WriteLine($"{name} is {age}.\n" +
+        $" {name} choose to order burger letter {A} for {payment}.\n" +
+        $" It's {Tr}, {name} had to pay {tax} percent for taxes.");
+        
+
+        
+     
+
+
+
+
+
+
+
+    }
+}
+
