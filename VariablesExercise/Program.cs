ddb51e855f5d0084fd4e037e3a5bcc9274b8fc19
Stephen Sellers <stephensellers034@yahoo.com>
2024-05-03 13:20:47 -0400

    added notes and new verison of solution

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string name = "John";
+        string name = "John";// declaring and initalizeing my variables
         int age = 34;
         char A = 'A';
         bool Tr = true;
@@ -13,11 +13,20 @@ class Program
 
         Console.WriteLine($"{name} is {age}.\n" +
         $" {name} choose to order burger letter {A} for {payment}.\n" +
-        $" It's {Tr}, {name} had to pay {tax} percent for taxes.");
-        
+        $" It's {Tr}, {name} had to pay {tax} percent for taxes.");// string interpolation to display my variables
 
-        
-     
+
+        string name1 = "Branden";
+        int age1 = 3;
+        char b = 'B';
+        bool day = false;
+        double weight = 25.86;
+        decimal tax1 = 0.8m;
+
+        Console.WriteLine($"{name1} turned {age1}.\n" +
+        $"{name1} was marked {b} for boy at birth and it's {day} that he was born on the 5th.\n" +
+        $"{name1} weigth after some time was {weight} and his car seat was expensive i felt like it was an {tax1} percent tax on his car seat.");
+     // my additons to retry what was done before
 
 
 
